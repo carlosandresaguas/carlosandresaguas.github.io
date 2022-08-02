4f569f3f381923a9294dd6d25a5129b8e9b63673
carlosandresaguas <carlosandresaguas99@gmail.com>
2022-08-01 23:05:43 -0500

    cambios8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "217737a882d8632b56977d6b0210dc88",
-"assets/assets/eci.docx": "613b4ba773f98f18b4c3701db41deba0",
+"assets/assets/eci.docx": "61e977528acc589a232712fb91073f3b",
 "assets/assets/ecm.docx": "afa44f9db61a17f6cc19eb5c63ead8d0",
 "assets/assets/ehi.docx": "6667a2ff78b93ca7ec41a0fc9b7e4788",
 "assets/assets/ehm.docx": "75181c04963d866c9c93fac3c5887c1f",
@@ -126,8 +126,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "435c9cd40f99f1a98831a4a0e2d50798",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ccb3cf19cc7eac8b18ebecc0c76ab555",
-"/": "ccb3cf19cc7eac8b18ebecc0c76ab555",
+"index.html": "e602c7307746b738153a70ea2e5ed3e9",
+"/": "e602c7307746b738153a70ea2e5ed3e9",
 "main.dart.js": "b008d694d54d118862c877af71e593c1",
 "manifest.json": "63ac8c62789497583d04b0e5c810ce5d",
 "version.json": "fe7bc1c922ddfe6558f97c2456ea02be"
